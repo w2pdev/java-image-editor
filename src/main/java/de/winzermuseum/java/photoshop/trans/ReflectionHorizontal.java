8b6f528104ac5965dbccff8572b5ca4501e3dda7
fred <leo.wipfler@gmail.com>
2023-11-23 17:29:22 +0100

    added functions

--- src/main/java/de/winzermuseum/java/photoshop/trans/ReflectionHorizontal.java
+++ src/main/java/de/winzermuseum/java/photoshop/trans/ReflectionHorizontal.java
@@ -17,30 +17,15 @@ public class ReflectionHorizontal implements Transformer
 
     final int[][] output = new int[numRows][numCols];
 
-    // TODO
+    for (int i = 0; i < numRows; i++){
+      for (int j = 0; j < numCols; j++){
+        output[i][numCols - (j +1)] = input [i] [j];
+
+      }
+    }
 
     return output;
   }
 }
 
 
-/*
-
-This file is part of the zone.informatik.java.graphics.trans package.
-
-Copyright (C) 2023 Matthias Ebert
-
-This package is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-The package is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with the package.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
