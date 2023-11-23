c5fa96fe80e5d339c8f165e924843e952787f7af
fred <leo.wipfler@gmail.com>
2023-11-23 17:29:07 +0100

    removed ebert

--- src/main/java/de/winzermuseum/java/photoshop/trans/ExtractorGreen.java
+++ src/main/java/de/winzermuseum/java/photoshop/trans/ExtractorGreen.java
@@ -24,23 +24,3 @@ public class ExtractorGreen implements Transformer
 }
 
 
-/*
-
-This file is part of the zone.informatik.java.graphics.trans package.
-
-Copyright (C) 2023 Matthias Ebert
-
-This package is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-The package is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with the package.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
