c93d0db9fe07c1a9f0e419503d6de35fda3e68fc
wipflele <wipflele@lmn.ohgw.de>
2023-12-13 09:49:13 +0100

    optimised code strukture for ultrawide displays

--- src/main/java/de/winzermuseum/java/photoshop/trans/ExtractorRed.java
+++ src/main/java/de/winzermuseum/java/photoshop/trans/ExtractorRed.java
@@ -2,32 +2,9 @@ package de.winzermuseum.java.photoshop.trans;
 
 
 /**
- * Extraktion des roten Farbkanals
+ *
  */
-public class ExtractorRed implements Transformer
-{
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public int[][] apply(final int[][] input )
-  {
-    final int numRows = input.length;
-    final int numCols = input[0].length;
-
-    final int[][] output = new int[numRows][numCols];
-
-
-    int Red=0;
-    for(int i=0; i<numCols; i++)
-    {
-      for(int j=0; j<numRows; j++)
-      {
-        Red = input [j][i];
-        Red = Red & 16711680 ;
-        output [j][i]= Red;
-      }
-    }
+public class ExtractorRed implements Transformer { @Override public int[][] apply(final int[][] input ) {final int numRows = input.length;final int numCols = input[0].length;final int[][] output = new int[numRows][numCols];int Red=0;for(int i=0; i<numCols; i++) {for(int j=0; j<numRows; j++) {Red = input [j][i];Red = Red & 16711680 ;output [j][i]= Red;}}
 
 
 
